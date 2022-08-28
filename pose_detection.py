bfc77fa4b32d1ad3f357c23025075bcf2bbe2f31
minukjeong <76443227+minukjeong@users.noreply.github.com>
2022-08-28 11:46:48 +0900

    Create pose_detection.py

---
+++ pose_detection.py
@@ -0,0 +1,99 @@
+import cv2
+import mediapipe as mp
+import time
+import math
+
+class poseDetector():
+    def __init__(self, mode = False,
+                 static_image_mode=False,
+                 smooth_landmarks=True,
+                 enable_segmentation=False,
+                 smooth_segmentation=True,
+                 min_detection_confidence=0.5,
+                 min_tracking_confidence=0.5):
+        self.mode = mode
+        self.static_image_mode = static_image_mode
+        self.smooth_landmarks = smooth_landmarks
+        self.enable_segmentation = enable_segmentation
+        self.smooth_segmentation = smooth_segmentation
+        self.min_detection_confidence = min_detection_confidence
+        self.min_tracking_confidence = min_tracking_confidence
+
+        #랜드마크 좌표값을 통해 점을 찍어주는 모듈
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode,self.static_image_mode,self.smooth_landmarks,
+                                self.enable_segmentation,self.smooth_segmentation,self.min_detection_confidence)
+
+    def findpose(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        #print(self.results.pose_landmarks)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,self.mpPose.POSE_CONNECTIONS)
+
+        return img
+
+    def findPosition(self, img, draw=True):
+        self.lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h, w, c = img.shape
+                # print(id, lm)
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+        return self.lmList
+
+    def findAngle(self, img, p1, p2, p3, draw=True):
+
+        # Get the landmarks
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        x3, y3 = self.lmList[p3][1:]
+
+        # Calculate the Angle
+        angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
+                             math.atan2(y1 - y2, x1 - x2))
+        print(angle)
+        if angle < 0:
+            angle += 360
+
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
+            cv2.line(img, (x3, y3), (x2, y2), (255, 255, 255), 3)
+            cv2.circle(img, (x1, y1), 10, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x1, y1), 15, (255, 0, 0), 2)
+            cv2.circle(img, (x2, y2), 10, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x2, y2), 15, (255, 0, 0), 2)
+            cv2.circle(img, (x3, y3), 10, (255, 0, 0), cv2.FILLED)
+            cv2.circle(img, (x3, y3), 15, (255, 0, 0), 2)
+            cv2.putText(img, str(int(angle)), (x2 - 50, y2 + 50),
+                        cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+
+        return angle
+
+
+def main():
+    cap = cv2.VideoCapture('C:/Users/gram/Desktop/image/bicepcurl.mp4')
+    pTime = 0
+    detector = poseDetector()
+    while True:
+        success, img = cap.read()
+        img = detector.findpose(img)
+        lmList = detector.findPosition(img, draw=False)
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+        cv2.imshow("", img)
+        cv2.waitKey(10)
+
+
+
+
+if __name__ == "__main__":
+    main()
